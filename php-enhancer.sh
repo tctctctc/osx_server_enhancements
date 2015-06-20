487b939c4542aee2bf54f4618c598d24e18cb08c
Michael Bagnall <mrbagnall@icloud.com>
2015-06-20 17:12:29 -0500

    Updated the shell script to use php-5-5-24

--- php-enhancer.sh
+++ php-enhancer.sh
@@ -8,10 +8,10 @@ tar -xzf jpegsrc.v6b.tar.gz
 tar -xzf libgd-GD_2_0_33.tar.gz
 tar -xzf libmcrypt-2.5.8.tar.gz
 tar -xzf libpng-1.6.16.tar.gz
-tar -xzf php-5.5.20.tar.gz
+tar -xzf php-5.5.24.tar.gz
 tar -xzf icu4c-4-8-1.tgz 
 
-cd php-5.5.20
+cd php-5.5.24
 ./configure '--prefix=/usr' '--mandir=/usr/share/man' '--infodir=/usr/share/info' '--sysconfdir=/private/etc' '--with-apxs2=/usr/sbin/apxs' '--enable-cli' '--with-config-file-path=/etc' '--with-config-file-scan-dir=/Library/Server/Web/Config/php' '--with-libxml-dir=/usr' '--with-openssl=/usr' '--with-kerberos=/usr' '--with-zlib=/usr' '--enable-bcmath' '--with-bz2=/usr' '--enable-calendar' '--disable-cgi' '--with-curl=/usr' '--enable-dba' '--with-ndbm=/usr' '--enable-exif' '--enable-fpm' '--enable-ftp' '--with-icu-dir=/usr' '--with-ldap=/usr' '--with-ldap-sasl=/usr' '--with-libedit=/usr' '--enable-mbstring' '--enable-mbregex' '--with-mysql=mysqlnd' '--with-mysqli=mysqlnd' '--without-pear' '--with-pear=no' '--with-pdo-mysql=mysqlnd' '--with-mysql-sock=/var/mysql/mysql.sock' '--with-readline=/usr' '--enable-shmop' '--with-snmp=/usr' '--enable-soap' '--enable-sockets' '--enable-sysvmsg' '--enable-sysvsem' '--enable-sysvshm' '--with-tidy' '--enable-wddx' '--with-xmlrpc' '--with-iconv-dir=/usr' '--with-xsl=/usr' '--enable-zip'
 make clean
 make
